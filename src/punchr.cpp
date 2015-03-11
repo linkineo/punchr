8dda1716c561451dadbe51676db38dcd49fa957e
Patrick Wieder <patrick.wieder@ch.sauter-bc.com>
2015-03-11 23:56:51 +0100

    INITIAL COMMIT

---
+++ src/punchr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "boost/program_options.hpp"
+
+#include "timesheet.h"
+
+
+
+void handleOptions(int argc, char ** argv)
+{
+namespace po = boost::program_options;
+po::variables_map vm;
+
+}
+
+
+int main(int argc, char** argv)
+{
+std::cout << "Test" << std::endl;
+}
+
+
